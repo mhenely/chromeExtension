486d10736a8c438e20f256ff47afa4d71f00c105
Snow Xue Bai <xueapp@gmail.com>
2022-12-14 20:10:26 -0800

    8pm

---
+++ main.js
@@ -0,0 +1,21 @@
+document.addEventListener('DOMContentLoaded', (e) => {
+  const title = document.createElement('h1');
+  title.innerText = 'Soccer Frenzy';
+  document.querySelector('body').appendChild(title);
+  // let myData = fetch('http://api.espn.com/v1/sports/baseball/mlb/events').then(data => data.json());
+  // myData.then(data => console.log(data));
+  
+// fetch ('https://api-football-v1.p.rapidapi.com/v3/timezone')
+//     .then((data) => data.json())
+//     .then((data) => {
+//        console.log(data);
+//     }
+//     )
+
+
+  
+
+})
+
+
+
